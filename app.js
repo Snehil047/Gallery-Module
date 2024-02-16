d286ab090a9af6631ba019e65ea22aa5fe4e418c
Snehil Maurya <snehilwilldi@gmail.com>
2024-02-16 13:24:23 +0530

    completed

---
+++ app.js
@@ -0,0 +1,24 @@
+let scrollContainer = document.querySelector(".gallery");
+let nextBtn = document.getElementById("nextBtn");
+let backBtn = document.getElementById("backBtn");
+let img = document.getElementById("1stImg");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+});
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 900;
+});
+
+// img.addEventListener("click", () => {
+//   img.style.width = "100%";
+//   img.style.height = "500px";
+// });
